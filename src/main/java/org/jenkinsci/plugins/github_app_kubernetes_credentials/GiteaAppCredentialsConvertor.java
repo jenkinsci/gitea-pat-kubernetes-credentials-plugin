b0f6a4ccea426cb5d64c5331eede50b478534f1c
ThisIsQasim <18313886+ThisIsQasim@users.noreply.github.com>
2022-04-02 23:35:30 +0500

    update

--- src/main/java/org/jenkinsci/plugins/github_app_kubernetes_credentials/GiteaAppCredentialsConvertor.java
+++ src/main/java/org/jenkinsci/plugins/github_app_kubernetes_credentials/GiteaAppCredentialsConvertor.java
@@ -1,11 +1,11 @@
-package org.jenkinsci.plugins.github_app_kubernetes_credentials;
+package org.jenkinsci.plugins.gitea_app_kubernetes_credentials;
 
 import com.cloudbees.jenkins.plugins.kubernetes_credentials_provider.CredentialsConvertionException;
 import com.cloudbees.jenkins.plugins.kubernetes_credentials_provider.SecretToCredentialConverter;
 import com.cloudbees.jenkins.plugins.kubernetes_credentials_provider.SecretUtils;
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import io.fabric8.kubernetes.api.model.Secret;
-import org.jenkinsci.plugins.github_branch_source.GitHubAppCredentials;
+import org.jenkinsci.plugin.gitea.credentials;
 import org.jenkinsci.plugins.variant.OptionalExtension;
 
 import java.nio.ByteBuffer;
@@ -19,24 +19,22 @@ import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@OptionalExtension(requirePlugins = {"github-branch-source", "kubernetes-credentials-provider"})
-public class GithubAppCredentialsConvertor extends SecretToCredentialConverter {
-    private static final Logger LOG = Logger.getLogger(GithubAppCredentialsConvertor.class.getName());
+@OptionalExtension(requirePlugins = {"gitea", "kubernetes-credentials-provider"})
+public class GiteaAppCredentialsConvertor extends SecretToCredentialConverter {
+    private static final Logger LOG = Logger.getLogger(GiteaAppCredentialsConvertor.class.getName());
 
     @Override
     public boolean canConvert(String type) {
-        return "githubApp".equals(type);
+        return "giteaApp".equals(type);
     }
 
     @Override
-    public GitHubAppCredentials convert(Secret secret) throws CredentialsConvertionException {
-        SecretUtils.requireNonNull(secret.getData(), "github app credential definition contains no data");
-        String appIdBase64 = SecretUtils.getNonNullSecretData(secret, "appId", "github app credential is missing appId");
-        String privateKeyBase64 = SecretUtils.getNonNullSecretData(secret, "privateKey", "github app credential is missing privateKey");
+    public GiteaAppCredentials convert(Secret secret) throws CredentialsConvertionException {
+        SecretUtils.requireNonNull(secret.getData(), "gitea app credential definition contains no data");
+        String appIdBase64 = SecretUtils.getNonNullSecretData(secret, "appId", "gitea app credential is missing appId");
         String appId = decodeBase64(appIdBase64, "Not a valid appId");
-        String privateKey = decodeBase64(privateKeyBase64, "Not a valid privateKey");
         hudson.util.Secret privateKeySecret = hudson.util.Secret.fromString(privateKey);
-        GitHubAppCredentials gitHubAppCredentials = new GitHubAppCredentials(
+        GiteaAppCredentials giteaAppCredentials = new GiteaAppCredentials(
                 // Scope
                 CredentialsScope.GLOBAL,
                 // ID
@@ -51,19 +49,19 @@ public class GithubAppCredentialsConvertor extends SecretToCredentialConverter {
         Optional<String> apiUrlOptional = SecretUtils.getOptionalSecretData(
                 secret,
                 "apiUri",
-                "github app credential : failed to retrieve optional parameter apiUri");
+                "gitea app credential : failed to retrieve optional parameter apiUri");
         if (apiUrlOptional.isPresent()) {
             String apiUrl = decodeBase64(apiUrlOptional.get(), "Not a valid apiUri");
-            gitHubAppCredentials.setApiUri(apiUrl);
+            giteaAppCredentials.setApiUri(apiUrl);
         }
         Optional<String> ownerOptional = SecretUtils.getOptionalSecretData(secret,
                 "owner",
-                "github app credential : failed to retrieve optional parameter owner");
+                "gitea app credential : failed to retrieve optional parameter owner");
         if (ownerOptional.isPresent()) {
             String owner = decodeBase64(ownerOptional.get(), "Not a valid apiUri");
-            gitHubAppCredentials.setOwner(owner);
+            giteaAppCredentials.setOwner(owner);
         }
-        return gitHubAppCredentials;
+        return giteaAppCredentials;
 
     }
 
